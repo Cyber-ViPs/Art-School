63ce2db1601aa854d232259f7eb5cb170fd3de83
Cyber-ViPs <johnyyd3@gmail.com>
2021-03-31 02:27:21 -0300

    v1

---
+++ cadastrar.py
@@ -0,0 +1,10 @@
+class Aluno:
+    nome = ""
+    telefone =""
+    email =""
+    av1 =0
+    av2=0
+    av3=0
+
+    def media(self):
+      return  media(self.av1+self.av2)/2
---
+++ w.aluno.py
@@ -0,0 +1,8 @@
+from cadastrar import Aluno
+
+aluno1 = Aluno()
+nome ="Jessica"
+telefone = "126548984"
+print(aluno1.nome)
+
+
